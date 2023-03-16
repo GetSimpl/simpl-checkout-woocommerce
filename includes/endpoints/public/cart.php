7040c447253d2fba5a718de695f713413bd444b7
Sathia <sathia.s@getsimpl.com>
2023-03-16 14:54:58 +0530

    fix unique device id

--- includes/endpoints/public/cart.php
+++ includes/endpoints/public/cart.php
@@ -4,13 +4,18 @@ include_once SIMPL_PLUGIN_DIR . "/includes/simpl_integration/simpl_integration.p
 include_once SIMPL_PLUGIN_DIR . "/includes/helpers/cart_helper.php";
 
 function create_cart( WP_REST_Request $request ) {
-    $productID = $request->get_params()["product_id"];
-    $variantID = $request->get_params()["variant_id"];
-    $quantity = $request->get_params()["quantity"];
     initCartCommon();
-    WC()->cart->add_to_cart($productID, $quantity, $variantID);
-    $si = new SimplIntegration();
+    if(isset($request->get_params()["is_pdp"]) && $request->get_params()["is_pdp"]) {
+        $productID = $request->get_params()["product_id"];
+        $variantID = $request->get_params()["variant_id"];
+        $quantity = $request->get_params()["quantity"];
+        WC()->cart->empty_cart();
+        WC()->cart->add_to_cart($productID, $quantity, $variantID);
+    } else {
+    }
+
     try {
+        $si = new SimplIntegration();
         return $si->cart_redirection_url(WC()->cart);
     } catch (Exception $fe) {        
         return new WP_Error("cart_creation_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
--- includes/simpl_integration/simpl_integration.php
+++ includes/simpl_integration/simpl_integration.php
@@ -28,7 +28,14 @@ class SimplIntegration {
         $response = array("source" => "cart");
         $checkout = $cart->checkout;
         foreach($cart_content as $item_id => $item) {
-            $response["unique_id"] = $item['key'];
+            $unique_device_id = WC()->session->get("simpl:session:id");
+            if($unique_device_id) {
+                $unique_device_id = WC()->session->get("simpl:session:id");
+            } else {
+                $unique_device_id = uniqid();
+                WC()->session->set("simpl:session:id", $unique_device_id);
+            }
+            $response["unique_id"] = $unique_device_id;
             $price = round($item['line_subtotal']*100) + round($item['line_subtotal_tax']*100);
             $cart_payload = array("total_price" => $price);
             $cart_payload["total_price"] = $price;
