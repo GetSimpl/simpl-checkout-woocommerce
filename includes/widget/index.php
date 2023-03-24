522b520fe9de9911d3cccbf60da065fbf1762715
srini <simpl@192.168.1.8>
2023-03-24 17:02:32 +0530

    rendering cta using plugin

--- includes/widget/index.php
+++ includes/widget/index.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-    include_once SIMPL_PLUGIN_DIR . '/includes/helpers/debug_helper.php';
-    include_once 'buy-now-button.php';
+include_once SIMPL_PLUGIN_DIR . '/includes/helpers/debug_helper.php';
+include_once SIMPL_PLUGIN_DIR . '/includes/helpers/widget_helper.php';
+include_once 'buy-now-button.php';
 
 ?>
