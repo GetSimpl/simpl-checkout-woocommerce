905b89026bb0fb70c557db82c96aa9cea27670cf
Sathia <sathia.s@getsimpl.com>
2023-04-06 15:51:42 +0530

    order api refactoring

--- includes/endpoints/internal/order.php
+++ includes/endpoints/internal/order.php
@@ -6,36 +6,37 @@ include_once SIMPL_PLUGIN_DIR . "/includes/helpers/wc_helper.php";
 
 
 function create_order( WP_REST_Request $request ) {
-    initCartCommon();
-    WC()->cart->empty_cart();
-    $order_id = $request->get_params()["checkout_order_id"];
-    $simpl_cart_token = $request->get_params()["simpl_cart_token"];
-    $simpl_payment_id = $request->get_params()["simpl_payment_id"];
-    if(!isset($simpl_cart_token) || $simpl_cart_token == "") {
-        return new WP_REST_Response(array("code"=> "bad_request", "message"=> "simpl_cart_token is mandatory"), 400);
-    }
-    if(!isset($simpl_payment_id) || $simpl_payment_id == "") {
-        return new WP_REST_Response(array("code"=> "bad_request", "message"=> "simpl_payment_id is mandatory"), 400);
-    }
-    $order = wc_get_order((int)$order_id);
-    WC()->session->order_awaiting_payment = $order->get_id();
-    WC()->session->set("simpl_order_id", $order_id);
-    $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
-    if($available_gateways["simpl"]) {
-        $order->update_meta_data("simpl_cart_token", $simpl_cart_token);
-        $order->update_meta_data("simpl_payment_id", $simpl_payment_id);
+    try {
+        initCartCommon();
+        $validation_errors = validate_order_request($request);
+        if(isset($validation_errors)) {
+            return $validation_errors;
+        }
+
+        $order = wc_get_order((int)$request->get_params()["checkout_order_id"]);
+        WC()->session->order_awaiting_payment = $order->get_id();
+        WC()->session->set("simpl_order_id", $order_id);
+        $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
+        if(!$available_gateways["simpl"]) {
+            return new WP_REST_Response(array("code"=> "bad_request", "message"=> "order already confirmed"), 400);
+        }
+        
+        $order->update_meta_data("simpl_cart_token", $request->get_params()["simpl_cart_token"]);
+        $order->update_meta_data("simpl_payment_id", $request->get_params()["simpl_payment_id"]);
+        $order->save();
         $result = $available_gateways["simpl"]->process_payment($order_id);
         WC()->session->set("simpl_order_id", null);
         WC()->session->set("simpl:session:id", null);
-        if($result["result"] == "success") {
-            $si = new SimplIntegration();
-            $order_payload = $si->order_payload($order);
-            $order_payload["order_status_url"] = $result["redirect"];
-            return $order_payload;
-        }
-    } else {
-        return new WP_REST_Response(array("code"=> "bad_request", "message"=> "order already confirmed"), 400);
-    }
 
-    return "";
+        if($result["result"] != "success") return new WP_Error("user_error", "order is not successful");
+
+        $si = new SimplIntegration();
+        $order_payload = $si->order_payload($order);
+        $order_payload["order_status_url"] = $result["redirect"];
+        return $order_payload;
+    } catch (Exception $fe) {
+        return new WP_Error("user_error", $fe->getMessage());
+    } catch (Error $fe) {
+        return new WP_Error("user_error", "error in creating order", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
+    }
 }
--- includes/helpers/validators.php
+++ includes/helpers/validators.php
@@ -23,6 +23,23 @@ function  validate_coupon_request($request) {
     return null;
 }
 
+function  validate_order_request($request) {
+    $errors = validate_checkout_order_id($request);
+    if(isset($errors)) {
+        return $errors;
+    }
+    
+    $simpl_cart_token = $request->get_params()["simpl_cart_token"];
+    $simpl_payment_id = $request->get_params()["simpl_payment_id"];
+    if(!isset($simpl_cart_token) || $simpl_cart_token == "") {
+        return new WP_REST_Response(array("code"=> "bad_request", "message"=> "simpl_cart_token is mandatory"), 400);
+    }
+    if(!isset($simpl_payment_id) || $simpl_payment_id == "") {
+        return new WP_REST_Response(array("code"=> "bad_request", "message"=> "simpl_payment_id is mandatory"), 400);
+    }    
+    return null;
+}
+
 function validate_line_items($request) {
     if(!isset($request->get_params()["items"]) || count($request->get_params()["items"]) == 0) {
         return new WP_REST_Response(array("code"=> "bad_request", "message"=> "items cannot be empty"), 400);
