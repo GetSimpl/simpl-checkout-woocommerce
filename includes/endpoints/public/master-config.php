929eade3a102c2f07019cb6fd3a84c1450c2eedf
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2023-04-11 14:24:02 +0530

    updaed with session id

--- includes/endpoints/public/master-config.php
+++ includes/endpoints/public/master-config.php
@@ -3,16 +3,21 @@
 function fetch_master_config( ) {
     $simpl_host = WC_Simpl_Settings::simpl_host();
     $store_url = WC_Simpl_Settings::store_url();
+    $unique_device_id = get_unique_device_id();
     $simplHttpResponse = wp_remote_get( "https://".$simpl_host."/api/v1/wc/widget/master-config?shop=".$store_url, array(
         "headers" => array(
-                "simpl-widget-session-token" => "",
+                "simpl-widget-session-token" => $unique_device_id,
                 "content-type" => "application/json"
             ),
     ));
 
     if ( ! is_wp_error( $simplHttpResponse ) ) {
         $body = json_decode( wp_remote_retrieve_body( $simplHttpResponse ), true );
-        if($body["success"]) {
+        $headers = wp_remote_retrieve_headers($simplHttpResponse);
+        if(isset($headers["simpl-widget-session-token"])){
+            set_unique_device_id($headers["simpl-widget-session-token"]);
+        }
+        if(isset($body["success"]) && isset($body["data"])) {
             echo("<script>var SimplMasterConfig = ". json_encode($body["data"]) ."</script>");
         } else {
             throw new Exception( $body['message'] );            
