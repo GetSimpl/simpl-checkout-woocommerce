9a675813505541c10be153645c4d307e616298cd
Sathia <sathia.s@getsimpl.com>
2023-04-12 21:11:54 +0530

    fix: address issue

--- includes/endpoints/internal/checkout.php
+++ includes/endpoints/internal/checkout.php
@@ -1,106 +1,83 @@
 <?php
-function create_checkout( WP_REST_Request $request ) {
+function create_checkout(WP_REST_Request $request)
+{
     try {
-        $validation_errors = validate_line_items($request);
-        if(isset($validation_errors)) {
-            return $validation_errors;
-
-        }
+        validate_line_items($request);
         $items = $request->get_params()["items"];
         initCartCommon();
-        
-        try {
-            add_to_cart($items);
-        } catch (CartCreationError $fe) {
-            return new WP_REST_Response(array("code"=> "bad_request", "message"=> $fe->getMessage()), 400);
-        }
-        
-        if(isset($request->get_params()["shipping_address"]) && isset($request->get_params()["billing_address"])) {
-            try {
-                set_address_in_cart($request->get_params()["shipping_address"], $request->get_params()["billing_address"]);
-            } catch (Exception $fe) {
-                return new WP_REST_Response(array("code"=> "bad_request", "message"=> $fe->getMessage()), 400);
-            }
+        add_to_cart($items);
+        if (isset($request->get_params()["shipping_address"]) && isset($request->get_params()["billing_address"])) {
+            set_address_in_cart($request->get_params()["shipping_address"], $request->get_params()["billing_address"]);
         }
         $order = create_order_from_cart();
         $si = new SimplIntegration();
         $cart_payload =  $si->cart_payload(WC()->cart, $order->get_id());
         do_action("simpl_abandoned_cart", WC()->cart, $cart_payload);
         return $cart_payload;
+    } catch (HttpBadRequest $fe) {
+        return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
     } catch (Exception $fe) {
         return new WP_Error("user_error", $fe->getMessage());
     } catch (Error $fe) {
         return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
-    }    
+    }
 }
 
-function update_checkout( WP_REST_Request $request ) {
+function update_checkout(WP_REST_Request $request)
+{
     try {
         $items = $request->get_params()["items"];
         initCartCommon();
-        $validation_errors = validate_shipping_address_or_items($request);
-        if(isset($validation_errors)) {
-            return $validation_errors;
-        }
-
-        $validation_errors = validate_checkout_order_id($request);
-        if(isset($validation_errors)) {
-            return $validation_errors;
-        }
-
-        if(isset($items) && count($items) > 0) {
-            $validation_errors = validate_line_items($request);
-            if(isset($validation_errors)) {
-                return $validation_errors;
-            }
-            try {
-                add_to_cart($items);
-            } catch (CartCreationError $fe) {
-                return new WP_REST_Response(array("code"=> "bad_request", "message"=> $fe->getMessage()), 400);
-            }    
+        validate_shipping_address_or_items($request);
+        validate_checkout_order_id($request);
+        if (isset($items) && count($items) > 0) {
+            validate_line_items($request);
+            add_to_cart($items);
         } else {
             $order_id = $request->get_params()["checkout_order_id"];
             load_cart_from_order($order_id);
-        }        
-
-        try {
-            set_address_in_cart($request->get_params()["shipping_address"], $request->get_params()["billing_address"]);
-        } catch (Exception $fe) {
-            return new WP_REST_Response(array("code"=> "bad_request", "message"=> $fe->getMessage()), 400);
         }
 
+        set_address_in_cart($request->get_params()["shipping_address"], $request->get_params()["billing_address"]);
         $order = update_order_from_cart($request->get_params()["checkout_order_id"]);
         $si = new SimplIntegration();
         $cart_payload = $si->cart_payload(WC()->cart, $order->id);
         do_action("simpl_abandoned_cart", WC()->cart, $cart_payload);
         return $cart_payload;
+    } catch (HttpBadRequest $fe) {
+        return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
     } catch (Exception $fe) {
         return new WP_Error("user_error", $fe->getMessage());
     } catch (Error $fe) {
         return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
-    }    
+    }
 }
 
-function fetch_checkout(WP_REST_Request $request) {
-    $validation_errors = validate_checkout_order_id($request);
-    if(isset($validation_errors)) {
-        return $validation_errors;
+function fetch_checkout(WP_REST_Request $request)
+{
+    try {
+        validate_checkout_order_id($request);
+        initCartCommon();
+        WC()->cart->empty_cart();
+        $order_id = $request->get_params()["checkout_order_id"];
+        $order = wc_get_order($order_id);
+        if ($order) {
+            convert_wc_order_to_wc_cart($order);
+            $si = new SimplIntegration();
+            return $si->cart_payload(WC()->cart, $order_id);
+        }
+    } catch (HttpBadRequest $fe) {
+        return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
+    } catch (Exception $fe) {
+        return new WP_Error("user_error", $fe->getMessage());
+    } catch (Error $fe) {
+        return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
     }
-    
-    initCartCommon();
-    WC()->cart->empty_cart();
-    $order_id = $request->get_params()["checkout_order_id"];
-    $order = wc_get_order($order_id);
-    if($order) {
-        convert_wc_order_to_wc_cart($order);
-        $si = new SimplIntegration();
-        return $si->cart_payload(WC()->cart, $order_id);
-    }
-    return new WP_REST_Response(array("code"=> "not_found", "message"=> "invalid checkout_order_id"), 404);
 }
 
-function internal_authenticate() {        
-    if(WC_Simpl_Settings::is_localhost()) {
+function internal_authenticate()
+{
+    if (WC_Simpl_Settings::is_localhost()) {
         return true;
     }
 
@@ -109,11 +86,13 @@ function internal_authenticate() {
     return $authenticated;
 }
 
-class CartCreationError extends Exception {
-    public function errorMessage() {
-      //error message
-      $errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
-      .':'.$this->getMessage();
-      return $errorMsg;
+class HttpBadRequest extends Exception
+{
+    public function errorMessage()
+    {
+        //error message
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile()
+            . ':' . $this->getMessage();
+        return $errorMsg;
     }
-  }
+}
--- includes/endpoints/internal/shipping-method.php
+++ includes/endpoints/internal/shipping-method.php
@@ -5,15 +5,16 @@ include_once SIMPL_PLUGIN_DIR . "/includes/helpers/cart_helper.php";
 include_once SIMPL_PLUGIN_DIR . "/includes/helpers/wc_helper.php";
 
 
-function set_shipping_method( WP_REST_Request $request ) {
+function set_shipping_method(WP_REST_Request $request)
+{
     initCartCommon();
     WC()->cart->empty_cart();
+    validate_shipping_method_request($request);
     $order_id = $request->get_params()["checkout_order_id"];
-    $shipping_method = $request->get_params()["shipping_method_id"];
     $order = wc_get_order((int)$order_id);
-    if($order) {
+    if ($order) {
         $cart = convert_wc_order_to_wc_cart($order);
-        WC()->session->set('chosen_shipping_methods', array( $shipping_method ));        
+        WC()->session->set('chosen_shipping_methods', array($shipping_method));
         update_shipping_line($order_id);
         WC()->cart->calculate_shipping();
         WC()->cart->calculate_totals();
