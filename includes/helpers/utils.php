bdece096c5226e715a35c642247c8bd4a12a6c5a
Simpl <simpl@SIMPL-ENG-050.local>
2023-04-13 20:29:17 +0530

    ranamed method to simpl_cart_init_common

--- includes/helpers/utils.php
+++ includes/helpers/utils.php
@@ -2,13 +2,13 @@
 
 
     function get_unique_device_id() {
-        initCartCommon();
+	    simpl_cart_init_common();
         $unique_device_id = WC()->session->get("simpl:session:id") ?: "";
         return $unique_device_id;
     }
 
     function set_unique_device_id($unique_device_id){
-        initCartCommon();
+	    simpl_cart_init_common();
         WC()->session->set("simpl:session:id", $unique_device_id);
     }
 
