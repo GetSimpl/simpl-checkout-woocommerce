6bbaad5a6f7fc41c631ba39c80a6068bf50bf064
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2023-04-13 12:43:34 +0530

    PR review comments

--- includes/helpers/utils.php
+++ includes/helpers/utils.php
@@ -1,13 +1,14 @@
 <?php
 
-    initCartCommon();
 
     function get_unique_device_id() {
+        initCartCommon();
         $unique_device_id = WC()->session->get("simpl:session:id") ?: "";
         return $unique_device_id;
     }
 
     function set_unique_device_id($unique_device_id){
+        initCartCommon();
         WC()->session->set("simpl:session:id", $unique_device_id);
     }
 
