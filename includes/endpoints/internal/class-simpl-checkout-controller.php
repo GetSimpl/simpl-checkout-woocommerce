22061187cebc556e541e43959f2335d9ada6c6f6
saurabh <saurabh.singh@getsimpl.com>
2023-04-17 11:21:44 +0530

    pulled main

--- includes/endpoints/internal/class-simpl-checkout-controller.php
+++ includes/endpoints/internal/class-simpl-checkout-controller.php
@@ -16,11 +16,11 @@ class SimplCheckoutController {
             do_action("simpl_abandoned_cart", WC()->cart, $cart_payload);
             return $cart_payload;
         } catch (HttpBadRequest $fe) {
-            return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
+            return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_BAD_REQUEST, "message" => $fe->getMessage()), 400);
         } catch (Exception $fe) {
-            return new WP_Error("user_error", $fe->getMessage());
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => $fe->getMessage()), 500);
         } catch (Error $fe) {
-            return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => 'error in creating checkout'), 500);
         }
     }
     
@@ -46,11 +46,11 @@ class SimplCheckoutController {
             do_action("simpl_abandoned_cart", WC()->cart, $cart_payload);
             return $cart_payload;
         } catch (HttpBadRequest $fe) {
-            return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
+            return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_BAD_REQUEST, "message" => $fe->getMessage()), 400);
         } catch (Exception $fe) {
-            return new WP_Error("user_error", $fe->getMessage());
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => $fe->getMessage()), 500);
         } catch (Error $fe) {
-            return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => 'error in creating checkout'), 500);
         }
     }
     
@@ -65,11 +65,11 @@ class SimplCheckoutController {
             $si = new SimplCartResponse();
             return $si->cart_payload(WC()->cart, $order_id);
         } catch (HttpBadRequest $fe) {
-            return new WP_REST_Response(array("code" => "bad_request", "message" => $fe->getMessage()), 400);
+            return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_BAD_REQUEST, "message" => $fe->getMessage()), 400);
         } catch (Exception $fe) {
-            return new WP_Error("user_error", $fe->getMessage());
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => $fe->getMessage()), 500);
         } catch (Error $fe) {
-            return new WP_Error("user_error", "error in creating checkout", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => 'error in creating checkout'), 500);
         }
     }
 }
--- includes/endpoints/internal/class-simpl-checkout-order-controller.php
+++ includes/endpoints/internal/class-simpl-checkout-order-controller.php
@@ -13,26 +13,29 @@ class SimplCheckoutOrderController {
             WC()->session->set("simpl_order_id", $order_id);
             $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
             if (!$available_gateways["simpl"]) {
-                return new WP_REST_Response(array("code" => "bad_request", "message" => "order already confirmed"), 400);
+                return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_BAD_REQUEST, "message" => "order already confirmed"), 400);
             }
     
             $order->update_meta_data("simpl_cart_token", $request->get_params()["simpl_cart_token"]);
             $order->update_meta_data("simpl_payment_id", $request->get_params()["simpl_payment_id"]);
             $order->save();
             $result = $available_gateways["simpl"]->process_payment($order_id);
+
+	        WC()->mailer()->customer_invoice( $order ); // Sending Invoice to Customer
+
             WC()->session->set("simpl_order_id", null);
             WC()->session->set("simpl:session:id", null);
     
-            if ($result["result"] != "success") return new WP_Error("user_error", "order is not successful");
+            if ($result["result"] != "success") return new WP_Error(SIMPL_HTTP_ERROR_USER_NOTICE, "order is not successful");
     
             $si = new SimplCartResponse();
             $order_payload = $si->order_payload($order);
             $order_payload["order_status_url"] = $result["redirect"];
             return $order_payload;
         } catch (Exception $fe) {
-            return new WP_Error("user_error", $fe->getMessage());
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => $fe->getMessage()), 500);
         } catch (Error $fe) {
-            return new WP_Error("user_error", "error in creating order", array("error_mesage" => $fe->getMessage(), "backtrace" => $fe->getTraceAsString()));
+	        return new WP_REST_Response(array("code" => SIMPL_HTTP_ERROR_USER_NOTICE, "message" => 'error in creating order'), 500);
         }
     }
 }
