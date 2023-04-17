c51094fe2f665c797c2c21e6ba5f2a0a88d8e623
Krunal Lakhatariya <krunal.lakhatariya@getsimpl.com>
2023-04-17 19:33:55 +0530

    PR changes completed

--- includes/helpers/utils.php
+++ includes/helpers/utils.php
@@ -11,17 +11,4 @@ function set_unique_device_id($unique_device_id){
 	WC()->session->set("simpl:session:id", $unique_device_id);
 }
 
-//This function will clear all type of notice or success
-function simpl_hide_error_messages()
-{
-
-	$_SESSION["simpl_session_message"] = [];
-	WC()->session->set('wc_notices', null);
-	add_filter( 'woocommerce_notice_types', '__return_empty_array' );
-	add_filter( 'wc_add_to_cart_message_html', '__return_false' );
-	add_filter( 'woocommerce_cart_item_removed_notice_type', '__return_false' );
-	add_filter( 'woocommerce_coupon_message', '' );
-	wc_clear_notices();
-}
-
 ?>
