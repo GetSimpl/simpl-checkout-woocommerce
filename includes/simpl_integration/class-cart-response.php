0970bea85fbb320fbe6a84ab95072e57fde79550
rithiksharma100 <rithik@getsimpl.com>
2023-05-17 14:22:06 +0530

    refactor

--- includes/simpl_integration/class-cart-response.php
+++ includes/simpl_integration/class-cart-response.php
@@ -124,22 +124,9 @@ class SimplCartResponse
     public function fetch_order_payload($order)
     {
         $response = array();
-        $response["legacy_resource_id"] = $order->get_id();
-        $is_order_paid = $order->is_paid();
-        $financial_status = 'PENDING';
-        if ($is_order_paid) {
-            $financial_status = 'PAID';
-        }
-        $is_order_confirmed = false;
-        $order_status = $order->get_status();
-        if ($order_status == 'processing' || $order_status == 'completed') {
-            $is_order_confirmed = true;
-        } else if ($order_status == 'refunded') {
-            $financial_status = 'REFUNDED';
-            $is_order_confirmed = true;
-        }
-        $response["confirmed"] = $is_order_confirmed;
-        $response["display_financial_status"] = $financial_status;
+        $response["id"] = $order->get_id();
+        $response["status"] = $order->get_status();
+        $response["is_paid"] = $order->is_paid();
         self::simpl_hide_error_messages(); // HIDE WOOCOMMERCE SUCCESS OR ERROR NOTIFICATION
         return $response;
     }
