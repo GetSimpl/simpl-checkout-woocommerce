38e83cc554cd3cf041cd9d72693f014c26e05dd0
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2023-09-05 15:23:28 +0530

    fix: add simpl-widget-session-token as cookie

--- includes/endpoints/public/master-config.php
+++ includes/endpoints/public/master-config.php
@@ -24,6 +24,7 @@ function fetch_master_config() {
 		$headers = wp_remote_retrieve_headers( $simplHttpResponse );
 		if ( isset( $headers[ $simplTokenHeader ] ) ) {
 			set_unique_device_id( $headers[ $simplTokenHeader ] );
+			setcookie($simplTokenHeader, $headers[ $simplTokenHeader ], time() + (1 * 365 * 24 * 60 * 60)); 
 		}
 		$masterConfigData = isset( $body["success"] ) && isset( $body["data"] ) ? json_encode( $body["data"] ) : '{}';
 		echo( '<script type="text/javascript">var SimplMasterConfig = ' . $masterConfigData . '</script>' );
