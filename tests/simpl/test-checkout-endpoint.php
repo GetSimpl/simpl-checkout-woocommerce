8150f67e137631bf7a9d01decd0815414557b832
rithiksharma100 <rithik@getsimpl.com>
2023-09-08 15:29:05 +0530

    fixed tests

--- tests/simpl/test-checkout-endpoint.php
+++ tests/simpl/test-checkout-endpoint.php
@@ -46,13 +46,13 @@ class Test_Checkout_Endpoint extends WP_UnitTestCase{
         $request = new WP_REST_Request( 'POST', '/wc-simpl/v1/checkout' );
         $request["items"] = array(array("product_id" => $data['product_id'], "variant_id" => $data['variant_id'], "quantity" => 1));
         $request["shipping_address"] = array("city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
-        $request["billing_address"] = array("email" => $user->get_email(), "city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
+        $request["billing_address"] = array("email" => "test@gmail.com", "city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
         $response = $this->server->dispatch( $request );
         $response_data = $response->get_data();
         $this->assertEquals($response_data["source"], "cart");
         $this->assertEquals($response_data["cart"]["total_price"], '10.00');
         $this->assertEquals($response_data["cart"]["shipping_address"], array("city"=> "chennai", "country" => "India", "state" => "Delhi"));
-        $this->assertEquals($response_data["cart"]["billing_address"], array("email" => $user->get_email(), "city"=> "chennai", "country" => "India", "state" => "Delhi"));
+        $this->assertEquals($response_data["cart"]["billing_address"], array("email" => "test@gmail.com", "city"=> "chennai", "country" => "India", "state" => "Delhi"));
         $this->assertEquals($response_data["cart"]["item_subtotal_price"], '10.00');
         $this->assertNotNull($response_data["cart"]["checkout_order_id"]);
 	}
