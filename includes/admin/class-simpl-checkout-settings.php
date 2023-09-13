5fd5997ed49cdd99d78608ba6ca0f28fded3d76f
Vikanksh Pundhir <vikankshpundhir@SIMPL-ENG-167.local>
2023-09-13 12:11:18 +0530

    PF-5 removing localhost

--- includes/admin/class-simpl-checkout-settings.php
+++ includes/admin/class-simpl-checkout-settings.php
@@ -70,9 +70,10 @@ class WC_Simpl_Settings {
 	}
 
 	public static function widget_script_url() {
-		if ( SIMPL_ENV == "localhost" ) {
-			return WIDGET_SCRIPT_LOCALHOST;
-		}
+		// if ( SIMPL_ENV == "localhost" ) {
+		// 	return WIDGET_SCRIPT_LOCALHOST;
+		// }
+		 // this is used only to run locally
 		$staging_env = get_option( "wc_settings_tab_simpl_test_env" );
 		if ( $staging_env == "yes" ) {
 			return WIDGET_SCRIPT_STAGING_URL;
