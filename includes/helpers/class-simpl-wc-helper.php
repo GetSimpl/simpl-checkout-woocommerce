bb0025d276343f2f3ac34681282b67cc828ed1e2
rithiksharma100 <rithik@getsimpl.com>
2023-09-13 15:16:17 +0530

    removed test logs

--- includes/helpers/class-simpl-wc-helper.php
+++ includes/helpers/class-simpl-wc-helper.php
@@ -167,14 +167,11 @@ class SimplWcCartHelper {
         $customer = simpl_get_customer_by_email($order->get_billing_email());
         if(empty($customer->get_id())) {
             $customer = self::set_new_customer_info_from_order($order);
-        } else {
-            print_r("EXISTING CUSTOMER");
         }
         $order->set_customer_id($customer->get_id());
     }
 
     static protected function set_new_customer_info_from_order($order) {
-        print_r("REGISTERING NEW CUSTOMER");
         $customer = WC()->customer;
         $customer->set_email($order->get_billing_email());
         $customer->set_first_name($order->get_shipping_first_name());
