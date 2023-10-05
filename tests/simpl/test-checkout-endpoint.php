52a996aa55113196f46a2526e78aeec3930372a2
rithiksharma100 <rithik@getsimpl.com>
2023-10-05 15:44:18 +0530

    added and updated tests

--- tests/simpl/test-checkout-endpoint.php
+++ tests/simpl/test-checkout-endpoint.php
@@ -45,7 +45,7 @@ class Test_Checkout_Endpoint extends WP_UnitTestCase{
         WC()->cart->add_to_cart($data['product_id'], 1, $data['variant_id']);
         $order = SimplWcCartHelper::create_order_from_cart();
         $request = new WP_REST_Request( 'POST', '/wc-simpl/v1/checkout' );
-        $request["items"] = array(array("product_id" => $data['product_id'], "variant_id" => $data['variant_id'], "quantity" => 1));
+        $request["items"] = array(array("product_id" => $data['product_id'], "variant_id" => $data['variant_id'], "quantity" => 1, "attributes" => array()));
         $request["shipping_address"] = array("city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
         $request["billing_address"] = array("email" => "test@gmail.com", "city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
         $request["merchant_additional_details"] = array(""=> "");
@@ -56,6 +56,29 @@ class Test_Checkout_Endpoint extends WP_UnitTestCase{
         $this->assertEquals($response_data["cart"]["shipping_address"], array("city"=> "chennai", "country" => "India", "state" => "Delhi"));
         $this->assertEquals($response_data["cart"]["billing_address"], array("email" => "test@gmail.com", "city"=> "chennai", "country" => "India", "state" => "Delhi"));
         $this->assertEquals($response_data["cart"]["item_subtotal_price"], '10.00');
+        $this->assertNull($response_data["cart"]["items"][0]["attributes"]);
+        $this->assertNotNull($response_data["cart"]["checkout_order_id"]);
+	}
+
+    public function test_create_checkout_with_valid_item_attributes() {
+        $data = create_product();
+        $user = create_test_user();        
+        simpl_cart_init_common();
+        WC()->cart->add_to_cart($data['product_id'], 1, $data['variant_id']);
+        $order = SimplWcCartHelper::create_order_from_cart();
+        $request = new WP_REST_Request( 'POST', '/wc-simpl/v1/checkout' );
+        $request["items"] = array(array("product_id" => $data['product_id'], "variant_id" => $data['variant_id'], "quantity" => 1, "attributes" => $data['variation']));
+        $request["shipping_address"] = array("city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
+        $request["billing_address"] = array("email" => "test@gmail.com", "city"=> "chennai", "country" => "india", "line1" => "123", "line2" => "456", "state" => "Delhi");
+        $request["merchant_additional_details"] = array(""=> "");
+        $response = $this->server->dispatch( $request );
+        $response_data = $response->get_data();
+        $this->assertEquals($response_data["source"], "cart");
+        $this->assertEquals($response_data["cart"]["total_price"], '10.00');
+        $this->assertEquals($response_data["cart"]["shipping_address"], array("city"=> "chennai", "country" => "India", "state" => "Delhi"));
+        $this->assertEquals($response_data["cart"]["billing_address"], array("email" => "test@gmail.com", "city"=> "chennai", "country" => "India", "state" => "Delhi"));
+        $this->assertEquals($response_data["cart"]["item_subtotal_price"], '10.00');
+        $this->assertEquals($response_data["cart"]["items"][0]["attributes"], $data["variation"]);
         $this->assertNotNull($response_data["cart"]["checkout_order_id"]);
 	}
 
@@ -69,7 +92,7 @@ class Test_Checkout_Endpoint extends WP_UnitTestCase{
 
     public function test_create_checkout_with_invalid_items_payload() {
         $request = new WP_REST_Request( 'POST', '/wc-simpl/v1/checkout' );
-        $request["items"] = array(array("product_id" => 121, "variant_id" => 112, "quantity" => 1));
+        $request["items"] = array(array("product_id" => 121, "variant_id" => 112, "quantity" => 1, "attributes" => array()));
         $response = $this->server->dispatch( $request );        
         $response_data = $response->get_data();
         $this->assertEquals($response_data["code"], "bad_request");
