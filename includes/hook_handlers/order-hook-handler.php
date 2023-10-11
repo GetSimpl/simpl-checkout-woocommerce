d2bc0abf22bfed4d77eeea7a9c786dddff6f24b0
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2023-10-11 16:21:56 +0530

    feat: add handler for wc order created hook

--- includes/hook_handlers/order-hook-handler.php
+++ includes/hook_handlers/order-hook-handler.php
@@ -1,13 +1,9 @@
 <?php
-function order_hook($order_id)
+function order_refunded_hook($order_id)
 {
     $order = wc_get_order($order_id);
 
-    $order_data = $order->get_data();
-    $order_data["line_items"] = SimplUtil::get_data($order->get_items());
-    $order_data["tax_lines"] = SimplUtil::get_data($order->get_taxes());
-    $order_data["shipping_lines"] = SimplUtil::get_data($order->get_shipping_methods());
-    $order_data["refunds"] = SimplUtil::get_data($order->get_refunds());
+    $order_data = fetch_order_data($order);
 
     $request["topic"] = "order.updated";
     $request["resource"] = "order";
@@ -20,4 +16,31 @@ function order_hook($order_id)
     } catch (\Throwable $th) { 
         error_log(print_r($th, TRUE)); 
     }
+}
+
+// pass following payload to 3pp
+// order create
+// cart create
+// cart update (or cart upsert)
+// raise event from 3pp using event publisher client for these payloads
+function woocommerce_checkout_order_created_hook($order_id, $posted_data, $order)
+{
+    $request["topic"] = "order.created";
+    $request["data"] = fetch_order_data($order);;
+
+    $checkout_3pp_client = new SimplCheckout3ppClient();
+    try {
+        $simplHttpResponse = $checkout_3pp_client->post_hook_request($request);
+    } catch (\Throwable $th) {
+        error_log(print_r($th, TRUE)); 
+    }
+}
+
+function fetch_order_data($order) {
+    $order_data = $order->get_data();
+    $order_data["line_items"] = SimplUtil::get_data($order->get_items());
+    $order_data["tax_lines"] = SimplUtil::get_data($order->get_taxes());
+    $order_data["shipping_lines"] = SimplUtil::get_data($order->get_shipping_methods());
+    $order_data["refunds"] = SimplUtil::get_data($order->get_refunds());
+    return $order_data;
 }
--- includes/hook_handlers/setup-hooks.php
+++ includes/hook_handlers/setup-hooks.php
@@ -1,2 +1,3 @@
 <?php
-    add_action( 'woocommerce_order_refunded', 'order_hook', 10, 1 );
+    add_action( 'woocommerce_order_refunded', 'order_refunded_hook', 10, 1 );
+    add_action( 'woocommerce_checkout_order_processed', 'woocommerce_checkout_order_created_hook', 10, 3);
