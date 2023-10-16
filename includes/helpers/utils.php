af1145cda8f738fdc0753667f23cf146b10777cb
Piyush Jain <piyush.j@getsimpl.com>
2023-10-17 00:28:29 +0530

    get_device_id on null session

--- includes/helpers/utils.php
+++ includes/helpers/utils.php
@@ -1,12 +1,16 @@
 <?php
 
 function get_unique_device_id() {
-	$unique_device_id = WC()->session->get("simpl:session:id") ?: "";
-	return $unique_device_id;
+	if(WC()->session) {
+		return WC()->session->get("simpl:session:id");
+	}
+	return "";
 }
 
 function set_unique_device_id($unique_device_id){
-	WC()->session->set("simpl:session:id", $unique_device_id);
+	if(WC()->session) {
+		WC()->session->set("simpl:session:id", $unique_device_id);
+	}
 }
 
 ?>
