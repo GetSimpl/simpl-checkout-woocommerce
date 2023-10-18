ed9792bf1f3f5aa62d57f82d8addb2fe2b14a578
Piyush Jain <piyush.j@getsimpl.com>
2023-10-18 10:25:03 +0530

    WooCommerce Debug helper

--- includes/widget/load.php
+++ includes/widget/load.php
@@ -1,6 +1,7 @@
 <?php 
 
-    include_once SIMPL_PLUGIN_DIR . '/includes/helpers/debug_helper.php';
+	include_once SIMPL_PLUGIN_DIR . '/includes/helpers/debug.php';
+	include_once SIMPL_PLUGIN_DIR . '/includes/helpers/debug_helper.php';
 	include_once SIMPL_PLUGIN_DIR . '/includes/helpers/utils.php';
 
 	if ( ! is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
