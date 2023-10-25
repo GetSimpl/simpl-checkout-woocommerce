2fd7bb89f8612ff9924c6a018f1acda200b8df0b
Harendra Chhekur <820121223505e@gmail.com>
2023-10-25 23:48:05 +0530

    :zap: [3ppClient] Remove: Unnecessary code

--- includes/clients/checkout-3pp-client.php
+++ includes/clients/checkout-3pp-client.php
@@ -3,6 +3,7 @@
 const SIMPL_SESSION_HEADER_KEY = 'simpl-widget-session-token';
 
 const GET_MASTER_CONFIG_PATH = 'https://%s/api/v1/wc/widget/master-config?shop=%s';
+const POST_HOOK_PATH = 'https://%s/wc/hook';
 
 //TODO: need to implement this proper client -> service -> controller way
 class SimplCheckout3ppClient {
@@ -20,8 +21,10 @@ class SimplCheckout3ppClient {
     function post_hook_request($request) {
         $request["merchant_client_id"] = $this->client_id;
         $request["store_url"] = $this->store_url;
+
+        $request_url = sprintf(POST_HOOK_PATH, $this->simpl_host);
         
-        $simplHttpResponse = wp_remote_post("https://" . $this->simpl_host . "/wc/hook", array(
+        $simplHttpResponse = wp_remote_post($request_url, array(
             "body" => json_encode($request),
             "headers" => array(
                 "content-type" => "application/json",
@@ -38,11 +41,11 @@ class SimplCheckout3ppClient {
         return $simplHttpResponse;
     }
 
-    function get_master_config() {
+    function get_master_config(WP_REST_Request $request) {
         $request_url = sprintf(GET_MASTER_CONFIG_PATH, $this->simpl_host, $this->store_url);
         $unique_device_id = '';
-        if ( function_exists( 'get_unique_device_id' ) ) {
-            $unique_device_id = get_unique_device_id() ?: "";
+        if ($request->get_header(SIMPL_SESSION_HEADER_KEY)) {
+            $unique_device_id = $request->get_header(SIMPL_SESSION_HEADER_KEY);
         }
 
         $resp = wp_remote_get( $request_url,
@@ -53,13 +56,18 @@ class SimplCheckout3ppClient {
                 ),
             )
         );
-        
+
         $body = json_decode( wp_remote_retrieve_body( $resp ), true );
         if ( ! is_wp_error( $resp ) ) {
-            if ( isset( $body["success"] ) && isset( $body["data"] ) ) {
-                return $body;
+            $response = new WP_REST_Response($body, 200);
+            $headers = wp_remote_retrieve_headers( $resp );
+            if ( isset( $headers[ SIMPL_SESSION_HEADER_KEY ] ) ) {
+                $response->header(SIMPL_SESSION_HEADER_KEY, $headers[ SIMPL_SESSION_HEADER_KEY ]);
             }
-            return array();
+            if ( isset( $body["success"] ) && isset( $body["data"] ) ) {
+                return $response;
+            }
+            return $response;
         } else {
             $error_message = $resp->get_error_message();
             scwp_console_log( $request_url );
