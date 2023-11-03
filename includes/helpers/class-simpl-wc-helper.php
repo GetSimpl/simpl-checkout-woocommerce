175b3640215a29932506d18ab38c05b000012026
rithiksharma100 <rithik@getsimpl.com>
2023-11-03 15:52:38 +0530

    removed conflicts

--- includes/helpers/class-simpl-wc-helper.php
+++ includes/helpers/class-simpl-wc-helper.php
@@ -25,10 +25,6 @@ class SimplWcCartHelper {
         $order->remove_order_items("line_item");
         WC()->checkout->create_order_line_items( $order, WC()->cart );
         self::set_address_in_order($order);
-<<<<<<< HEAD
-=======
-        $order->calculate_totals();
->>>>>>> main
         $order->recalculate_coupons();
         $order->save();
         return $order;
