bc93c472a997752c00c17beccab6cc7295810702
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2023-11-07 17:45:40 +0530

    fix: add req validation for required additional fields

--- includes/endpoints/public/v2/class-simpl-checkout-cart-controller.php
+++ includes/endpoints/public/v2/class-simpl-checkout-cart-controller.php
@@ -2,6 +2,8 @@
 
 class SimplCheckoutCartControllerV2 {
     function create(WP_REST_Request $request) {
+        wc_clear_notices();
+        
         foreach ( $request->get_params() as $key => $value ) {
             $_REQUEST[$key] = $value;
             $_POST[$key] = $value;
@@ -9,6 +11,19 @@ class SimplCheckoutCartControllerV2 {
 
         WC()->cart->empty_cart();
         WC_Form_Handler::add_to_cart_action();
+
+        $err = wc_get_notices('error');
+        if (isset($err) && count($err) > 0) {
+            $err_messages = array();
+            foreach ($err as $err_message) {
+                array_push($err_messages, strip_tags($err_message["notice"]));
+            }
+            
+            return new WP_REST_Response(array("success"=> false, "code" => SIMPL_HTTP_ERROR_CART_CREATE, "errors" => $err_messages), 400);
+        }
+
+
+
         try {
             $si = new SimplCartResponse();
             return array('redirection_url'=>$si->cart_redirection_url(WC()->cart, $request));
