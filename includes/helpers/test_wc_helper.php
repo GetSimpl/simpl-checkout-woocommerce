f2626079887b27d67bec4dd19927da81f04506b0
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2023-11-08 16:57:25 +0530

    fix: dep issue

--- includes/helpers/test_wc_helper.php
+++ includes/helpers/test_wc_helper.php
@@ -1,5 +1,5 @@
 <?php
-    use Automattic\WooCommerce\StoreApi\Utilities\OrderController;
+    require_once __DIR__ . '/wp-load.php';
     
     function simpl_test_create_product() {
         $product = new WC_Product_Variable();
