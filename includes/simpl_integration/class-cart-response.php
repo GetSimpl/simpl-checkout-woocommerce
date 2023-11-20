8ea28c121e05e449dcf75a289c841466d005016c
Abhay Maniyar <abhay.maniyar@getsimpl.com>
2023-11-20 17:36:30 +0530

    fix: update version to 1.3.2

--- includes/simpl_integration/class-cart-response.php
+++ includes/simpl_integration/class-cart-response.php
@@ -2,9 +2,9 @@
 
 const DEFAULT_WC_ORDER_FIELDS = ["key", "product_id", "variation_id", "variation", "quantity", "data", "data_hash", "line_tax_data", "line_subtotal", "line_subtotal_tax", "line_total", "line_tax"];
 
-const HEADERUSERAGENT = "User-Agent";
-const HEADERPLATFORM = "Platform";
-const HEADERORIGIN = "Origin";
+const HEADER_USER_AGENT = "User-Agent";
+const HEADER_PLATFORM = "Platform";
+const HEADER_ORIGIN = "Origin";
 
 class SimplCartResponse
 {
@@ -12,7 +12,7 @@ class SimplCartResponse
     {
         $merchant_additional_details = $request['merchant_additional_details'];
 
-        $client_information = get_client_information($request);
+        $client_information = self::get_client_information($request);
         $merchant_additional_details["client_information"] = $client_information;
 
         $cart_request = self::static_cart_payload($cart, $merchant_additional_details);
@@ -43,17 +43,17 @@ class SimplCartResponse
 
     public function get_client_information($request) {
         $client_information = [];
-        $header_user_agent = $request->get_header(HEADERUSERAGENT);
+        $header_user_agent = $request->get_header(HEADER_USER_AGENT);
         if ($header_user_agent != "") {
             $client_information["user_agent"] = $header_user_agent;
         }
 
-        $header_platform = $request->get_header(HEADERPLATFORM);
+        $header_platform = $request->get_header(HEADER_PLATFORM);
         if ($header_platform != "") {
             $client_information["platform"] = $header_platform;
         }
 
-        $header_origin = $request->get_header(HEADERORIGIN);
+        $header_origin = $request->get_header(HEADER_ORIGIN);
         if ($header_origin != "") {
             $client_information["origin"] = $header_origin;
         }
